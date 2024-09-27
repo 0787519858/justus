594cb8082d5f37abdc8d65de2d417c93fbd34db5
0787519858 <144285729+0787519858@users.noreply.github.com>
2024-09-27 14:16:48 +0300

    Add files via upload

---
+++ Receipt/Form1.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        DataGridView1 = New DataGridView()
+        Column1 = New DataGridViewTextBoxColumn()
+        Column2 = New DataGridViewTextBoxColumn()
+        Column3 = New DataGridViewTextBoxColumn()
+        Button1 = New Button()
+        PrintDocument1 = New Printing.PrintDocument()
+        Button2 = New Button()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Columns.AddRange(New DataGridViewColumn() {Column1, Column2, Column3})
+        DataGridView1.Location = New Point(290, 149)
+        DataGridView1.Margin = New Padding(6, 6, 6, 6)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 82
+        DataGridView1.Size = New Size(773, 267)
+        DataGridView1.TabIndex = 0
+        ' 
+        ' Column1
+        ' 
+        Column1.HeaderText = "ITEM"
+        Column1.MinimumWidth = 10
+        Column1.Name = "Column1"
+        Column1.Width = 200
+        ' 
+        ' Column2
+        ' 
+        Column2.HeaderText = "QTY"
+        Column2.MinimumWidth = 10
+        Column2.Name = "Column2"
+        Column2.Width = 200
+        ' 
+        ' Column3
+        ' 
+        Column3.HeaderText = "PRICE"
+        Column3.MinimumWidth = 10
+        Column3.Name = "Column3"
+        Column3.Width = 200
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(478, 508)
+        Button1.Margin = New Padding(6, 6, 6, 6)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(221, 92)
+        Button1.TabIndex = 1
+        Button1.Text = "Print"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(811, 554)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(150, 46)
+        Button2.TabIndex = 2
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' form1
+        ' 
+        AutoScaleDimensions = New SizeF(13F, 32F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1486, 960)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(DataGridView1)
+        Margin = New Padding(6, 6, 6, 6)
+        Name = "form1"
+        Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents PrintDocument1 As Printing.PrintDocument
+    Friend WithEvents Button2 As Button
+
+End Class
---
+++ Receipt/Form1.vb
@@ -0,0 +1,64 @@
+﻿Imports System.Drawing.Printing
+
+Public Class form1
+    Dim WithEvents PD As New PrintDocument
+    Dim PPD As New PrintPreviewDialog
+    Dim Longpaper As Integer
+
+    Private Sub PD_Beginprint(sender As Object, e As Printing.PrintEventArgs) Handles PD.BeginPrint
+        Dim Pagesetup As New PageSettings
+        Pagesetup.PaperSize = New PaperSize("Custom", 250, 500)
+        PD.DefaultPageSettings = Pagesetup
+    End Sub
+    Private Sub PD_PrintPage(Sender As Object, e As PrintPageEventArgs) Handles PD.PrintPage
+        Dim f8 As New Font("Calibri", 8, FontStyle.Regular)
+        Dim f10 As New Font("Calibri", 10, FontStyle.Regular)
+        Dim f10b As New Font("Calibri", 10, FontStyle.Bold)
+        Dim f14 As New Font("Calibri", 14, FontStyle.Bold)
+
+        Dim leftmargin As Integer = PD.DefaultPageSettings.Margins.Left
+        Dim centermargin As Integer = PD.DefaultPageSettings.PaperSize.Width / 2
+        Dim rightmargin As Integer = PD.DefaultPageSettings.PaperSize.Width
+
+        'font aligment
+        Dim right As New StringFormat
+        Dim center As New StringFormat
+
+        right.Alignment = StringAlignment.Far
+        center.Alignment = StringAlignment.Center
+
+        Dim line As String
+        line = "--------------------------------------------------------------"
+        e.Graphics.DrawString("Chine Town", f14, Brushes.Black, centermargin, 5, center)
+        e.Graphics.DrawString("New York Street 17 Avenue Garden", f10, Brushes.Black, centermargin, 25, center)
+        e.Graphics.DrawString("Tel +256785924788 ", f8, Brushes.Black, centermargin, 40, center)
+
+        e.Graphics.DrawString("Invoice ID", f8, Brushes.Black, 0, 60)
+        e.Graphics.DrawString(":", f8, Brushes.Black, 50, 60)
+        e.Graphics.DrawString("KGFS1654", f8, Brushes.Black, 75, 60)
+
+        e.Graphics.DrawString("Cashier", f8, Brushes.Black, 0, 75)
+        e.Graphics.DrawString(":", f8, Brushes.Black, 50, 75)
+        e.Graphics.DrawString("JUSTUS", f8, Brushes.Black, 70, 75)
+
+        e.Graphics.DrawString("08/19/2024", f8, Brushes.Black, 0, 90)
+
+        e.Graphics.DrawString(line, f8, Brushes.Black, 0, 100)
+
+        Dim height As Integer 'DGV Position
+        Dim i As Long
+        DataGridView1.AllowUserToAddRows = False
+        For row As Integer = 0 To DataGridView1.RowCount = 1
+            height += 15
+        Next
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        PPD.Document = PD
+        PPD.ShowDialog()
+
+    End Sub
+End Class
+
+
